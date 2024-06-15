43dd248c00ce7e63d189bdb382d8b212e06b4828
Ragib Al Asad <ragibalasad@gmail.com>
2024-06-16 00:07:21 +0600

    better config

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,13 @@
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname")
+class Settings(BaseSettings):
+    database_url: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
